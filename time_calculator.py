1f11ea19c5a99b733b56d38a2a83f8fe6083712f
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-07-24 14:45:29 +0700

    done: remove whitespaces

--- time_calculator.py
+++ time_calculator.py
@@ -18,7 +18,7 @@ def add_time(start, duration, day=None):
     old_hr = int(start.split()[0].split(':')[0])
     old_mn = int(start.split()[0].split(':')[1])
     period =  start.split()[1]
-    
+
     new_hr = int(duration.split(':')[0])
     new_mn = int(duration.split(':')[1])
     n_days = int(new_hr / 24)
@@ -30,7 +30,6 @@ def add_time(start, duration, day=None):
         mn %= 60
 
     p_cyle = int((old_hr + new_hr) / 12)
-
     hr = (old_hr + new_hr) % 12
 
     if mn < 10:
@@ -41,7 +40,7 @@ def add_time(start, duration, day=None):
 
     if period == 'PM' and old_hr + (new_hr % 12) >= 12:
         n_days += 1
-    
+
     if p_cyle % 2 == 1:
         period = periods[period]
 
@@ -51,7 +50,7 @@ def add_time(start, duration, day=None):
         index = int((days[day.lower()] + n_days) % 7)
         day = list(days.keys())[list(days.values()).index(int(index))]
         new_time += ', ' + day.capitalize()
-    
+
     if n_days == 1:
         return f'{new_time} (next day)'
     elif n_days > 1:
