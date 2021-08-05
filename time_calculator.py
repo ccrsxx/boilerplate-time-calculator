5e39de579b9a614ea4da0fe09993e9d9e04a2391
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2021-08-05 18:36:49 +0700

    another method: 24 to 12

--- time_calculator.py
+++ time_calculator.py
@@ -1,4 +1,4 @@
-def add(start_time, new_time, day=None):
+def add_time(start_time, new_time, day=None):
 
     days = {
         'monday': 0,
@@ -10,18 +10,24 @@ def add(start_time, new_time, day=None):
         'sunday': 6
     }
 
-    periods = {
-        'AM': 'PM',
-        'PM': 'AM'
-    }
-
-    old_hour = start_time.split(':')[0]
-    old_minute = start_time.split(':')[1].split()[0]
+    old_hour = int(start_time.split(':')[0])
+    old_minute = int(start_time.split(':')[1].split()[0])
     old_period = start_time.split()[-1]
 
-    new_hour = new_time.split(':')[0]
-    new_minute = new_time.split(':')[1]
-    n_days = int((old_hour + new_hour) / 24)
+    new_hour = int(new_time.split(':')[0])
+    new_minute = int(new_time.split(':')[1])
+
+    period = None
+    n_days = 0
+
+    if old_hour == 12 and old_period == 'AM':
+        old_hour = 0
+
+    elif old_hour == 12 and old_period == 'PM':
+        pass
+
+    elif old_hour >= 1 and old_period == 'PM':
+        old_hour += 12
 
     hour = old_hour + new_hour
     minute = old_minute + new_minute
@@ -30,6 +36,39 @@ def add(start_time, new_time, day=None):
         hour += 1
         minute %= 60
 
-    
+    if hour >= 24:
+        n_days = int(hour / 24)
+        hour %= 24
 
-add('1:00 AM', '2')
+    if minute < 10:
+        minute = f'0{minute}'
+
+    if not hour:
+        hour = 12
+        period = 'AM'
+
+    if hour < 12:
+        period = 'AM'
+ 
+    elif hour == 12 and not period:
+        hour = 12
+        period = 'PM'
+
+    elif hour >= 1 and not period:
+        hour -= 12
+        period = 'PM'
+
+    new_time = f'{hour}:{minute} {period}'
+
+    if day:
+        index = (days[day.lower()] + n_days) % 7
+        day = list(days.keys())[list(days.values()).index(index)]
+        new_time += f', {day.capitalize()}'
+
+    if n_days == 1:
+        return f'{new_time} (next day)'
+
+    elif n_days > 1:
+        return f'{new_time} ({n_days} days later)'
+
+    return new_time
